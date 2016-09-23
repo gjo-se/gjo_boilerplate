23ae9759626b7564a31e736e498122c90eb3c221
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2016-09-23 06:57:45 +0200

    added EXT-Basics

---
+++ ext_emconf.php
@@ -0,0 +1,28 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Boilerplate',
+    'description' => '',
+    'category' => 'misc',
+    'author' => 'Gregory Jo Erdmann',
+    'author_email' => 'gregory.jo@gjo-se.com',
+    'author_company' => 'gjo-se.com',
+	'shy' => 0,
+	'version' => '1.0.0',
+    'priority' => '',
+    'state' => 'alpha',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'lockType' => '',
+    'clearCacheOnLoad' => 1,
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.6.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+);
