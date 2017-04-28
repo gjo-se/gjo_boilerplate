c146e78ac46d2edff7628b45be42e4971b40a06d
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2017-04-28 08:32:03 +0200

    add Viewhelper

---
+++ Classes/ViewHelpers/Widget/Controller/PaginateController.php
@@ -0,0 +1,31 @@
+<?php
+namespace Gjo\GjoBoilerplate\ViewHelpers\Widget\Controller;
+
+/***************************************************************
+ *  created: 26.09.16 - 12:05
+ *  Copyright notice
+ *  (c) 2016 Gregory Jo Erdmann <gregory.jo@gjo-se.com>
+ *  All rights reserved
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Fluid\ViewHelpers\Widget\Controller\PaginateController as FluidPaginateController;
+
+/**
+ * Class PaginateController
+ * @package Gjo\GjoBoilerplate\ViewHelpers\Widget\Controller
+ */
+class PaginateController extends FluidPaginateController
+{
+}
