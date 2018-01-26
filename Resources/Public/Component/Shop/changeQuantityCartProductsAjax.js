985a5c4a41dfad6871a912c7201dcd8072627992
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2018-01-26 17:02:26 +0100

    added changeQuantityCartProductsAjax

---
+++ Resources/Public/Component/Shop/changeQuantityCartProductsAjax.js
@@ -0,0 +1,42 @@
+(function ($) {
+    'use strict';
+    var typeNum = 1516967943;
+
+    var quantityInput = $('.quantity');
+
+    var _changeQuantityCartProducts = function (openOrderUid, orderProductUid, quantity) {
+
+        $.ajax({
+            url: '/index.php',
+            method: 'POST',
+            data: {
+                type: typeNum,
+                openOrderUid: openOrderUid,
+                orderProductUid: orderProductUid,
+                quantity: quantity
+            },
+            success: function (response) {
+                _countCartProducts();
+            },
+            error: function (error) {
+                console.error(error);
+            }
+        });
+    };
+
+    $(document).ready(function () {
+
+        $(quantityInput).change(function (event) {
+
+            var openOrderUid = $(this).attr('data-order-uid');
+            var orderProductUid = $(this).attr('data-orderproduct-uid');
+            var quantity = $(this).val();
+
+            console.log('openOrderUid: ' + openOrderUid);
+            console.log('orderProductUid: ' + orderProductUid);
+
+            _changeQuantityCartProducts(openOrderUid, orderProductUid, quantity);
+        });
+    });
+
+})(jQuery);
