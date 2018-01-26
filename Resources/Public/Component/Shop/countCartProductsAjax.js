bb8d4dad2aa3fb37468ee139efa6a04f308f75a1
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2018-01-26 17:02:52 +0100

    added countCartProductsAjax

---
+++ Resources/Public/Component/Shop/countCartProductsAjax.js
@@ -0,0 +1,27 @@
+var _countCartProducts = function () {
+    var typeNum = 1516964175;
+
+    $.ajax({
+        url: '/index.php',
+        method: 'POST',
+        data: {
+            type: typeNum
+        },
+        success: function (response) {
+
+            var responseObj = jQuery.parseJSON(response);
+            var openOrderCountProducts = responseObj.openOrderCountProducts;
+
+            _setOpenOrderCountProducts(openOrderCountProducts);
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+
+    var _setOpenOrderCountProducts = function (openOrderCountProducts) {
+        $('.openOrderCountProducts').html('(' + openOrderCountProducts + ')')
+    };
+};
+
+_countCartProducts();
