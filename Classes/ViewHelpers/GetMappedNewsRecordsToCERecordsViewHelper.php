1878734715ba1598f0181941a257802d4f42571f
Gregory Jo Erdmann <gregory.jo@gjo-se.com>
2018-08-15 09:17:55 +0200

    added GetMappedNewsrecordsToCERecordsViewHelper

---
+++ Classes/ViewHelpers/GetMappedNewsRecordsToCERecordsViewHelper.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace GjoSe\GjoBoilerplate\ViewHelpers;
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\Exception;
+
+class GetMappedNewsRecordsToCERecordsViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+
+    public function initializeArguments()
+    {
+        $this->registerArgument(
+            'newsRecords',
+            'TYPO3\CMS\Extbase\Persistence\Generic\QueryResult',
+            'News Records',
+            true
+        );
+    }
+
+    public function render()
+    {
+        if (!isset($this->arguments['newsRecords'])) {
+            throw new Exception('Attribute "newsRecords" missing for GetMappedViewRecordsToCERecordsViewHelper');
+        }
+
+        $newsRecords = $this->arguments['newsRecords'];
+        $mappedNewsrecords = array();
+
+        if($newsRecords){
+
+            foreach ($newsRecords as $key => $newsRecord) {
+
+                $mappedNewsrecords[$key]['data']['isNews'] = true;
+                $mappedNewsrecords[$key]['data']['header'] = $newsRecord->getTitle();
+                $mappedNewsrecords[$key]['data']['bodytext'] = $newsRecord->getTeaser();
+                $mappedNewsrecords[$key]['data']['button_text'] = 'Zum Artikel';
+
+
+                //        - button_link // siehe dazu aktuellen Linkaufbau!!
+                //        - Detailseite mit UID
+
+                $mappedNewsrecords[$key]['images'] = $newsRecord->getFalMedia();
+
+            }
+        }
+
+//        \TYPO3\CMS\Extbase\Utility\DebuggerUtility::var_dump($mappedNewsrecords, '$mappedNewsrecords');
+
+
+        return $mappedNewsrecords;
+    }
+}
